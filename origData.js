be1c1a1b10af5e03b8e8ad7d0898d2702298d92a
Or Adler <ora@fieldbit.net>
2016-10-21 20:04:35 +0300

    init commit

---
+++ origData.js
@@ -0,0 +1,369 @@
+var catIems ={
+
+    "categories": [
+        {
+
+                "items": [
+                    {
+                        "id": "gdijnib8",
+                        "in_stock": false,
+                        "modifier_groups_count": 2,
+                        "modifier_groups_id": ['eMiy4iR4','kMT85Tay'],//items 1:N modifierGroups
+                        "name": "Tunami",
+                        "open": false,
+                        "price": 799,
+                        "price_levels": [
+                            {
+                                "id": "L8ijGiGX",
+                                "price": 799
+                            }
+                        ]
+                    },
+                    {
+                        "id": "rMTAbTjr",
+                        "in_stock": false,
+                        "modifier_groups_count": 2,
+                        "modifier_groups_id": ['eMiy4iR4','kMT85Tay'],
+                        "name": "New Bacon-ings",
+                        "open": false,
+                        "price": 689,
+                        "price_levels": [
+                            {
+                                "id": "7oTd8T9A",
+                                "price": 689
+                            }
+                        ]
+                    },
+                    {
+                        "id": "gkc8bca9",
+                        "in_stock": false,
+                        "modifier_groups_count": 2,
+                        "modifier_groups_id": ['eMiy4iR4','kMT85Tay'],
+                        "name": "Poblano Picasso Burger",
+                        "open": false,
+                        "price": 799,
+                        "price_levels": [
+                            {
+                                "id": "K6czEc8b",
+                                "price": 799
+                            }
+                        ]
+                    }
+                ]
+            ,
+            "id": "rXcXyc7b",
+            "name": "Burgers"
+        },
+        {
+
+                "items": [
+                    {
+                        "id": "gki84ia9",
+                        "in_stock": true,
+                        "modifier_groups_count": 0,
+                        "modifier_groups_id": [],
+                        "name": "Soda",
+                        "open": false,
+                        "price": 150,
+                        "price_levels": [
+                            {
+                                "id": "Byineidy",
+                                "price": 150
+                            },
+                            {
+                                "id": "g4T4dTBj",
+                                "price": 200
+                            },
+                            {
+                                "id": "K6czkc8b",
+                                "price": 250
+                            }
+                        ]
+                    },
+                    {
+                        "id": "doTaLTyg",
+                        "in_stock": true,
+                        "modifier_groups_count": 0,
+                        "name": "Orange Juice",
+                        "open": false,
+                        "price": 175,
+                        "price_levels": [
+                            {
+                                "id": "L4iqKid8",
+                                "price": 175
+                            },
+                            {
+                                "id": "K6T8MTzb",
+                                "price": 300
+                            }
+                        ]
+                    }
+                ]
+            ,
+            "id": "AdiRjiAp",
+            "name": "Drinks"
+        },
+        {
+
+                "items": [
+                    {
+                        "id": "GyiKLiLo",
+                        "in_stock": false,
+                        "modifier_groups_count": 0,
+                        "modifier_groups_id": [],
+                        "name": "Chips & Salsa",
+                        "open": false,
+                        "price": 250,
+                        "price_levels": [
+                            {
+                                "id": "neiani8B",
+                                "price": 250
+                            }
+                        ]
+                    },
+                    {
+                        "id": "recb5cKX",
+                        "in_stock": true,
+                        "modifier_groups_count": 0,
+                        "modifier_groups_id": [],
+                        "name": "Mozzarella Sticks",
+                        "open": false,
+                        "price": 425,
+                        "price_levels": [
+                            {
+                                "id": "Bycnrcdy",
+                                "price": 425
+                            }
+                        ]
+                    }
+                ]
+           ,
+            "id": "AkT69Tgy",
+            "name": "Appetizers"
+        }
+    ]
+};
+
+
+var modifiers = {
+
+
+        "modifiers": [
+            {
+                "id": "zRcEkcj8",
+                "name": "Overcooked",
+                "open": false,
+                "price_levels": [
+                    {
+                        "id": "REiAoiq7",
+                        "price": 0
+                    }
+                ],
+                "price_per_unit": 0
+
+            },
+            {
+                "id": "E5cpac84",
+                "name": "Tomato",
+                "open": false,
+                "price_levels": [
+                    {
+                        "id": "Rzi98iRR",
+                        "price": 0
+                    }
+                ],
+                "price_per_unit": 0
+
+            },
+            {
+                "id": "E5ip4i84",
+                "name": "Bleeding",
+                "open": false,
+                "price_levels": [
+                    {
+                        "id": "rgTxpTRk",
+                        "price": 0
+                    }
+                ],
+                "price_per_unit": 0
+
+            },
+            {
+                "id": "pAia8ioe",
+                "name": "Almost burnt",
+                "open": false,
+                "price_levels": [
+                    {
+                        "id": "AoTpRTdk",
+                        "price": 0
+                    }
+                ],
+                "price_per_unit": 0
+
+            },
+            {
+                "id": "GkiREiyL",
+                "name": "Cheese",
+                "open": false,
+                "price_levels": [
+                    {
+                        "id": "aoT55T4B",
+                        "price": 100
+                    }
+                ],
+                "price_per_unit": 100
+
+            },
+            {
+                "id": "8kT9KTX7",
+                "name": "Perfect",
+                "open": false,
+                "price_levels": [
+                    {
+                        "id": "nAceac8B",
+                        "price": 0
+                    }
+                ],
+                "price_per_unit": 0
+
+            },
+            {
+                "id": "4GT5XTze",
+                "name": "Burnt",
+                "open": false,
+                "price_levels": [
+                    {
+                        "id": "nGcLkcrx",
+                        "price": 0
+                    }
+                ],
+                "price_per_unit": 0
+
+            }
+        ]
+
+};
+
+var modifierGruop = {
+
+
+
+        "modifier_groups": [
+            {
+                "id": "eMiy4iR4",
+                "maximum": 1,
+                "minimum": 1,
+                "name": "Temperature",
+                "required": false,
+
+
+                    "options": [//looks like modifierGroups 1:N modifiers
+                        {
+                            "id": "4GT5XTze",
+                            "name": "Burnt",
+                            "open": false,
+                            "price_levels": [
+                                {
+                                    "id": "nGcLkcrx",
+                                    "price": 0
+                                }
+                            ],
+                            "price_per_unit": 0
+
+                        },
+                        {
+                            "id": "E5ip4i84",
+                            "name": "Bleeding",
+                            "open": false,
+                            "price_levels": [
+                                {
+                                    "id": "rgTxpTRk",
+                                    "price": 0
+                                }
+                            ],
+                            "price_per_unit": 0
+
+                        },
+                        {
+                            "id": "8kT9KTX7",
+                            "name": "Perfect",
+                            "open": false,
+                            "price_levels": [
+                                {
+                                    "id": "nAceac8B",
+                                    "price": 0
+                                }
+                            ],
+                            "price_per_unit": 0
+
+                        },
+                        {
+                            "id": "zRcEkcj8",
+                            "name": "Overcooked",
+                            "open": false,
+                            "price_levels": [
+                                {
+                                    "id": "REiAoiq7",
+                                    "price": 0
+                                }
+                            ],
+                            "price_per_unit": 0
+
+                        },
+                        {
+                            "id": "pAia8ioe",
+                            "name": "Almost burnt",
+                            "open": false,
+                            "price_levels": [
+                                {
+                                    "id": "AoTpRTdk",
+                                    "price": 0
+                                }
+                            ],
+                            "price_per_unit": 0
+
+                        }
+                    ]
+
+            },
+            {
+                "id": "kMT85Tay",
+                "maximum": null,
+                "minimum": 1,
+                "name": "Toppings",
+                "required": false,
+
+
+                    "options": [
+                        {
+                            "id": "E5cpac84",
+                            "name": "Tomato",
+                            "open": false,
+                            "price_levels": [
+                                {
+                                    "id": "Rzi98iRR",
+                                    "price": 0
+                                }
+                            ],
+                            "price_per_unit": 0
+
+                        },
+                        {
+                            "id": "GkiREiyL",
+                            "name": "Cheese",
+                            "open": false,
+                            "price_levels": [
+                                {
+                                    "id": "aoT55T4B",
+                                    "price": 100
+                                }
+                            ],
+                            "price_per_unit": 100
+
+                        }
+                    ]
+
+            }
+        ]
+
+};
